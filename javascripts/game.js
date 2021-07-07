1b30aa07d889c283e22cfa7b9ac8f55c8b428c90
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-07-07 12:59:41 +0200

    Adding

--- javascripts/game.js
+++ javascripts/game.js
@@ -5,18 +5,21 @@ class Game {
 
   newTurn() {
     this.turnLeft = this.turnLeft - 1;
-    const t = new Turn
-    // if (this.turnLeft == 0) {
-
-    // } else {
-    //   Turn.new;
-    // }
+    if (this.turnLeft > 0) {
+      this.newTurn()
+      console.log(`New turn ${this.turnLeft}`)
+    } else {
+      console.log("Game Over")
+    }
   }
 
 }
 
 const grace = new Fighter()
 const ulder = new Paladin()
+const moana = new Monk()
+const draven = new Berserker()
+const carl = new Assassin()
 console.log(grace,ulder)
 
 const gameNew = new Game
