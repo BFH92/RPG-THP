f6dc310bcfe70ce14306699d3835166a49da42ca
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-07-08 14:48:17 +0200

    prompt which attacker

--- javascripts/character.js
+++ javascripts/character.js
@@ -24,12 +24,6 @@ class Character {
     
   }
 
-
-    
-    // if (victim.hp <= 0) {
-    //   this.mana = this.mana + 20;
-    // }
-
   specialAttack(player, input) {
     if (player instanceof Assassin) {
       player.shadowHit(players[input]);
@@ -47,12 +41,27 @@ class Character {
   attackOf(player) {
     // console.log(`${player.name} joue`);
     // console.log(players.indexOf(player)); //FIXME: voir comment remplacer l'index du joueur comme input par le nom du joueur
+    var playersName =[]
+    players.map((p)=> 
+        {
+          if(p.name != player.name) {
+          playersName.push(p.name)
+          }
+        } 
+        
+        )
+
     var input = Number(prompt(
-      `${(player.name).toUpperCase()}: wich player do you want to attack?
-      [0] = XXX
-      [1] = XXX
-      [2] = XXX
+      `${(player.name).toUpperCase()}: wich player do you want to attack? 
+
+      ${shuffle.indexOf(shuffle[0])} - ${shuffle[0].name}  
+      ${shuffle.indexOf(shuffle[1])} - ${shuffle[1].name}
+      ${shuffle.indexOf(shuffle[2])} - ${shuffle[2].name}
+    
+    
+
     `));
+    //TODO: contenu statique
 
     if (this.validInput(input, player)) {
       var attackInput = prompt(
